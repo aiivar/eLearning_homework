70e9c698a225ee3f743560e1bc13101850d9bf6a
Aivar Minsafin <min.aivar2001@gmail.com>
2020-03-22 20:02:46 +0300

    added all homework up to and including 9

--- eLearning/src/ru/kpfu/itis/j903/cw/minsafin/Main.java
+++ eLearning/src/ru/kpfu/itis/j903/cw/minsafin/Main.java
@@ -6,28 +6,43 @@ import ru.kpfu.itis.j903.cw.minsafin.inf_6.MyMap;
 import ru.kpfu.itis.j903.cw.minsafin.inf_6.setformap.FromArrayListSet;
 import ru.kpfu.itis.j903.cw.minsafin.inf_7.StreamApiHomework;
 import ru.kpfu.itis.j903.cw.minsafin.inf_8.InputOutputTest;
+import ru.kpfu.itis.j903.cw.minsafin.inf_9.StudentByteBufferReaderWriter;
+import ru.kpfu.itis.j903.cw.minsafin.inf_9.student.BirthDate;
+import ru.kpfu.itis.j903.cw.minsafin.inf_9.student.Student;
 
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.Set;
+import java.io.File;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        streamApiTest();
+        byteBufferTest();
     }
 
-    public static void streamApiTest(){
+    public static void byteBufferTest() {
+        Student s1 = new Student(new BirthDate(12, 12, 1999), "Aivar Minsafin",11903);
+        Student s2 = new Student(new BirthDate(10, 5, 1923), "Nikita Nikitich",11903);
+        Student s3 = new Student(new BirthDate(21, 4, 3000), "Nikita ???????",11903);
+        List<Student> students= new ArrayList<Student>();
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        StudentByteBufferReaderWriter.write(students);
+        Collection<Student> students1 = StudentByteBufferReaderWriter.read(new File(StudentByteBufferReaderWriter.path));
+        System.out.println(students1);
+    }
+
+    public static void streamApiTest() {
         StreamApiHomework.execute();
     }
 
-    public static void myMapTest(){
+    public static void myMapTest() {
         Set<Map.Entry<Integer, String>> set = new FromArrayListSet<>();
         AbstractMap.SimpleEntry<Integer, String> entry = new AbstractMap.SimpleEntry<>(2, "Hello");
         set.add(entry);
-        MyMap<Integer,String> myMap = new MyMap<>(set);
+        MyMap<Integer, String> myMap = new MyMap<>(set);
         System.out.println(myMap.values());
-        MyMap<Integer,String> myMap2 = new MyMap<>();
+        MyMap<Integer, String> myMap2 = new MyMap<>();
         myMap2.put(2, "Hi");
         myMap2.put(3, "Test");
         myMap2.put(3, "Deleted");
@@ -48,7 +63,7 @@ public class Main {
         System.out.println(set);
     }
 
-    public static void IOTest(){
+    public static void IOTest() {
         InputOutputTest inputOutputTest = new InputOutputTest();
         inputOutputTest.execute();
     }
