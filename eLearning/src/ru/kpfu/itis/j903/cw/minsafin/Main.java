a7d6b9e91bbbc94a55afb0e45d4a8dcbb9f75561
Aivar Minsafin <min.aivar2001@gmail.com>
2020-03-22 13:58:49 +0300

    Stream API homework was added (HM#7)

--- eLearning/src/ru/kpfu/itis/j903/cw/minsafin/Main.java
+++ eLearning/src/ru/kpfu/itis/j903/cw/minsafin/Main.java
@@ -2,12 +2,36 @@ package ru.kpfu.itis.j903.cw.minsafin;
 
 import ru.kpfu.itis.j903.cw.minsafin.inf_5.mynavigableset.IntComparator;
 import ru.kpfu.itis.j903.cw.minsafin.inf_5.mynavigableset.MyNavigableSet;
+import ru.kpfu.itis.j903.cw.minsafin.inf_6.MyMap;
+import ru.kpfu.itis.j903.cw.minsafin.inf_6.setformap.FromArrayListSet;
+import ru.kpfu.itis.j903.cw.minsafin.inf_7.StreamApiHomework;
 import ru.kpfu.itis.j903.cw.minsafin.inf_8.InputOutputTest;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        IOTest();
+        streamApiTest();
+    }
+
+    public static void streamApiTest(){
+        StreamApiHomework.execute();
+    }
+
+    public static void myMapTest(){
+        Set<Map.Entry<Integer, String>> set = new FromArrayListSet<>();
+        AbstractMap.SimpleEntry<Integer, String> entry = new AbstractMap.SimpleEntry<>(2, "Hello");
+        set.add(entry);
+        MyMap<Integer,String> myMap = new MyMap<>(set);
+        System.out.println(myMap.values());
+        MyMap<Integer,String> myMap2 = new MyMap<>();
+        myMap2.put(2, "Hi");
+        myMap2.put(3, "Test");
+        myMap2.put(3, "Deleted");
+        System.out.println(myMap2.values());
     }
 
     public static void myNavSetTest() {
